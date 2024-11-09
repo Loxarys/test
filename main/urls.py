0a6eb4bc5a8e7e2e81bcdb70307c5acd1e658f79
Javier G <ja.gonzalezn@duocuc.cl>
2024-11-08 23:56:54 -0300

    vista formulario

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('',principal, name='principal'),
+    path('horas',horas, name='horas'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 def principal(request):
 
     return (render(request,'Landing.html'))
+
+def horas(request):
+    return (render(request,'horas.html'))
